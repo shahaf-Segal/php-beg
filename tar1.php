cb8623fd002247a3991ed9e620124ab1a84c003e
shahaf-Segal <shahaf100@outlook.co.il>
2025-01-15 00:20:00 +0200

    first commit

---
+++ tar1.php
@@ -0,0 +1,6 @@
+<?php
+echo "123 ", "hello" . "!\n";
+$name = "any";
+
+$number = 1;
+$number2 = 2;
---
+++ tempExerc.php
@@ -0,0 +1,4 @@
+<?php
+$celsius = 0;
+$fahrenheit =  $celsius * 9 / 5  + 32;
+echo $celsius . "C is " . $fahrenheit . "F";
